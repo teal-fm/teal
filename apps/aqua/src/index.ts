275af641f79b720f6442379396b7fc12f8d589ca
Kyle Loveless <kyle@loveless.dev>
2024-11-14 01:04:45 -0600

    xrpc agent roadblock

--- apps/aqua/src/index.ts
+++ apps/aqua/src/index.ts
@@ -1,10 +1,14 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { eq } from "drizzle-orm";
 import { db } from "@teal/db/connect";
+import { tealSession } from "@teal/db/schema";
 import { getAuthRouter, loginGet } from "./auth/router";
 import pino from "pino";
 import { EnvWithCtx, setupContext } from "./ctx";
 import { env } from "./lib/env";
+import { Agent } from "@atproto/api";
+import { getCookie } from "hono/cookie";
 
 const logger = pino({ name: "server start" });
 
@@ -12,7 +16,22 @@ const app = new Hono<EnvWithCtx>();
 
 app.use((c, next) => setupContext(c, db, logger, next));
 
-app.get("/", (c) => c.text("Hono meets Node.js"));
+app.get("/", async (c) => {
+  const cookies = getCookie(c, "tealSession");
+  const sessCookie = cookies?.split("teal:")[1];
+  console.log(`sess-id: ${sessCookie}`);
+
+  if (sessCookie != undefined) {
+    const session = await db.query.tealSession.findMany({
+      where: eq(tealSession.key, sessCookie),
+    });
+    console.log(`sessions: ${session.session}`);
+    const agent = new Agent(session.session);
+    console.log(agent.did);
+    return agent.getProfile({ actor: agent.did });
+  }
+  return c.text("Hono meets Node.js");
+});
 
 app.get("/info", async (c) => {
   const result = await db.query.status.findFirst().execute();
@@ -33,7 +52,11 @@ const run = async () => {
       },
       (info) => {
         logger.info(
-          `Listening on ${info.address == "::1" ? "http://localhost" : info.address}:${info.port} (${info.family})`,
+          `Listening on ${
+            info.address == "::1"
+              ? "http://localhost"
+              : "http://" + info.address
+          }:${info.port} (${info.family})`,
         );
       },
     );
