b430bddcf4a875e65cbc85342440d9ff68708c2f
Kyle Loveless <kyle@loveless.dev>
2024-11-16 14:52:33 -0600

    simple gui, move login to /login, cleanup

--- apps/aqua/src/auth/router.ts
+++ apps/aqua/src/auth/router.ts
@@ -3,51 +3,8 @@ import { db } from "@teal/db/connect";
 import { EnvWithCtx, TealContext } from "@/ctx";
 import { Hono } from "hono";
 import { tealSession } from "@teal/db/schema";
-
 import { setCookie } from "hono/cookie";
 import { env } from "@/lib/env";
-
-interface LoginBody {
-  handle?: string;
-}
-
-export const login = async (c: TealContext) => {
-  let body: LoginBody = await c.req.json();
-  // Initiate the OAuth flow
-  if (!body) return Response.json({ error: "Could not parse body" });
-  // handle is the handle of the user
-  if (!body.handle && body.handle === undefined) {
-    return Response.json({ error: "Handle is required" });
-  }
-  try {
-    const url = await atclient.authorize(body.handle, {
-      scope: "atproto transition:generic",
-      state: crypto.randomUUID(),
-    });
-    return Response.json({ redirect_to: url });
-  } catch (e) {
-    console.error(e);
-    return Response.json({ error: "Could not authorize user" });
-  }
-};
-
-export async function loginGet(c: TealContext) {
-  const handle = c.req.param("handle");
-  console.log("handle", handle);
-  // Initiate the OAuth flow
-  try {
-    console.log("Calling authorize");
-    const url = await atclient.authorize(handle, {
-      scope: "atproto transition:generic",
-    });
-    console.log("Redirecting to oauth login page");
-    console.log(url);
-    return Response.redirect(url);
-  } catch (e) {
-    console.error(e);
-    return Response.json({ error: "Could not authorize user" });
-  }
-}
 
 export async function callback(c: TealContext) {
   // Initiate the OAuth flow
@@ -92,10 +49,6 @@ export async function callback(c: TealContext) {
 }
 
 const app = new Hono<EnvWithCtx>();
-
-app.get("/login/:handle", async (c) => loginGet(c));
-
-app.post("/login", async (c) => login(c));
 
 app.get("/callback", async (c) => callback(c));
 
--- apps/aqua/src/index.ts
+++ apps/aqua/src/index.ts
@@ -3,11 +3,11 @@ import { Hono } from "hono";
 import { db } from "@teal/db/connect";
 import { getAuthRouter } from "./auth/router";
 import pino from "pino";
-import { EnvWithCtx, setupContext,  } from "./ctx";
+import { EnvWithCtx, setupContext, TealContext } from "./ctx";
 import { env } from "./lib/env";
 import { getCookie } from "hono/cookie";
 import { atclient } from "./auth/client";
-import { getContextDID, getUserInfo } from "./lib/auth";
+import { getContextDID, getSessionAgent, getUserInfo } from "./lib/auth";
 
 const logger = pino({ name: "server start" });
 
@@ -25,16 +25,91 @@ app.get("/", async (c) => {
   const cookies = getCookie(c, "tealSession");
   const sessCookie = cookies?.split("teal:")[1];
 
+  // Serve logged in content
   if (sessCookie != undefined) {
     const session = await getContextDID(c);
 
     if (session != undefined) {
-      return c.json(await getUserInfo(c));
+      const profile = await getUserInfo(c);
+      // const agent = await getSessionAgent(c);
+      // const followers = await agent?.getFollowers();
+      return c.html(
+        `<div id="root">
+          <div id="header">
+            <h1>teal.fm</h1>
+            <p>Your music, beautifully tracked. (soon.)</p>
+          </div>
+          <div class="container">
+            <h1>${profile?.handle}</h1>
+          </div>
+        </div>`
+      );
     }
   }
 
   // Serve non-logged in content
-  return c.text("teal-fm");
+  return c.html(
+    `<div id="root">
+    <div id="header">
+      <h1>teal.fm</h1>
+      <p>Your music, beautifully tracked. (soon.)</p>
+    </div>
+    <div class="container">
+      <button><a href="/login">Login</a></button>
+      <div class="signup-cta">
+        Don't have an account on the Atmosphere?
+        <a href="https://bsky.app">Sign up for Bluesky</a> to create one now!
+      </div>
+    </div>
+  </div>`
+  );
+});
+
+app.get("/login", (c) => {
+  return c.html(
+    `<div id="root">
+    <div id="header">
+      <h1>teal.fm</h1>
+      <p>Your music, beautifully tracked. (soon.)</p>
+    </div>
+    <div class="container">
+      <form action="/login" method="post" class="login-form">
+        <input
+          type="text"
+          name="handle"
+          placeholder="Enter your handle (eg alice.bsky.social)"
+          required
+        />
+        <button type="submit">Log in</button>
+      </form>
+      <div class="signup-cta">
+        Don't have an account on the Atmosphere?
+        <a href="https://bsky.app">Sign up for Bluesky</a> to create one now!
+      </div>
+    </div>
+  </div>`
+  );
+});
+
+app.post("/login", async (c: TealContext) => {
+  const body = await c.req.parseBody();
+  const { handle } = body;  
+  console.log("handle", handle);
+  // Initiate the OAuth flow
+  try {
+    console.log("Calling authorize");
+    if (typeof handle === "string") {
+      const url = await atclient.authorize(handle, {
+        scope: "atproto transition:generic",
+      });
+      console.log("Redirecting to oauth login page");
+      console.log(url);
+      return Response.redirect(url);
+    }
+  } catch (e) {
+    console.error(e);
+    return Response.json({ error: "Could not authorize user" });
+  }
 });
  
 const run = async () => {
