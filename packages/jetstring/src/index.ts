ee6d0bb36a0f4b43b3c3945edb98e3344bee6159
natalie <22222885+espeon@users.noreply.github.com>
2024-11-28 15:03:11 -0600

    statuses should get committed to db correctly now

--- packages/jetstring/src/index.ts
+++ packages/jetstring/src/index.ts
@@ -3,6 +3,11 @@ import { db } from "@teal/db/connect";
 import { status } from "@teal/db/schema";
 import { CommitCreateEvent, Jetstream } from "@skyware/jetstream";
 
+import {
+  Record as XyzStatusphereStatus,
+  isRecord as isStatusphereStatus,
+} from "@teal/lexicons/generated/server/types/xyz/statusphere/status";
+
 class Handler {
   private static instance: Handler;
   private constructor() {}
@@ -13,16 +18,27 @@ class Handler {
     return Handler.instance;
   }
 
-  handle(msg_type: string, msg: any) {
+  handle(msg_type: string, record: CommitCreateEvent<string & {}>) {
     // Handle message logic here
+    const msg = record.commit.record;
     console.log("Handling" + msg_type + "message:", msg);
-    if (msg_type === "xyz.statusphere.status") {
-      // serialize message as xyz.statusphere.status
-      const st = db.insert(status).values({
-        status: msg.status,
-        uri: msg.uri,
-        authorDid: msg.authorDid,
-      });
+    if (isStatusphereStatus(msg) && msg.$type === "xyz.statusphere.status") {
+      if (record.commit.operation === "create") {
+        // serialize message as xyz.statusphere.status
+        db.insert(status).values({
+          createdAt: new Date().getSeconds().toString(),
+          indexedAt: new Date(record.time_us).getSeconds().toString(),
+          status: msg.status,
+          // the AT path
+          uri: record.commit.rkey,
+          authorDid: record.did,
+        });
+      } else {
+        console.log("unsupported operation:", record.commit.operation);
+      }
+    } else {
+      console.log("Unknown message type:", msg_type);
+      console.log("Message:", record);
     }
   }
 }
