29663272b84ffbb98522906583bfd0e0371a1b91
natalie <22222885+espeon@users.noreply.github.com>
2024-12-24 22:46:11 -0600

    update database, lint issues, etc.

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "sqlite",
-  schema: "./packages/db/schema.ts",
-  out: "./packages/db/.drizzle",
+  schema: "./schema.ts",
+  out: "./.drizzle",
   casing: "snake_case",
   dbCredentials: {
     url: process.env.DATABASE_URL ?? "./db.sqlite",
