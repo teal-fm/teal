e558c90de60e2345adaf80421341f66b43683f48
Natalie <nat@natalie.sh>
2025-01-19 22:01:31 -0600

    oauth changes!

--- apps/amethyst/lib/atp/oauth.tsx
+++ apps/amethyst/lib/atp/oauth.tsx
@@ -19,41 +19,44 @@ export default function createOAuthClient(
   }
   let meta: ClientMetadata;
 
-    const isWeb = Platform.OS === "web";
-    const u = new URL(baseUrl);
-    let hostname = u.hostname;
-    if (hostname == "localhost") {
-      hostname = "127.0.0.1";
+  const isWeb = Platform.OS === "web";
+  const u = new URL(baseUrl);
+  let hostname = u.hostname;
+  if (hostname == "localhost") {
+    hostname = "127.0.0.1";
+  }
+  let redirect = `${u.protocol}//${hostname}`;
+  if (u.port !== "") {
+    redirect = `${redirect}:${u.port}`;
+  }
+  if (isWeb) {
+    redirect = `${redirect}/auth/callback`;
+  } else {
+    const scheme = Constants.expoConfig?.scheme;
+    if (!scheme) {
+      throw new Error("unable to resolve scheme for oauth redirect");
     }
-    let redirect = `${u.protocol}//${hostname}`;
-    if (u.port !== "") {
-      redirect = `${redirect}:${u.port}`;
-    }
-    if (isWeb) {
-      redirect = `${redirect}/auth/callback`;
-    } else {
-      const scheme = Constants.expoConfig?.scheme;
-      if (!scheme) {
-        throw new Error("unable to resolve scheme for oauth redirect");
-      }
-      redirect = `${redirect}/app-return/${scheme}`;
-    }
-    const queryParams = new URLSearchParams();
-    queryParams.set("scope", "atproto transition:generic");
-    queryParams.set("redirect_uri", redirect);
-    meta = {
-      client_id: `http://localhost?${queryParams.toString()}`,
-      redirect_uris: [redirect as any],
-      scope: "atproto transition:generic",
-      token_endpoint_auth_method: "none",
-      client_name: "Loopback client",
-      response_types: ["code"],
-      grant_types: ["authorization_code", "refresh_token"],
-      // > There is a special exception for the localhost development workflow [ ... ]
-      // > These clients use web URLs, but have application_type set to native in the generated client metadata.
-      application_type: "native",
-      dpop_bound_access_tokens: true,
-    };
+    redirect = `${redirect}/app-return/${scheme}`;
+  }
+  const queryParams = new URLSearchParams();
+  queryParams.set("scope", "atproto transition:generic");
+  queryParams.set("redirect_uri", redirect);
+  meta = {
+    client_id:
+      hostname === "localhost"
+        ? `http://localhost?${queryParams.toString()}`
+        : `https://${hostname}/client-metadata.json`,
+    redirect_uris: [redirect as any],
+    scope: "atproto transition:generic",
+    token_endpoint_auth_method: "none",
+    client_name: "Amethyst",
+    response_types: ["code"],
+    grant_types: ["authorization_code", "refresh_token"],
+    // > There is a special exception for the localhost development workflow [ ... ]
+    // > These clients use web URLs, but have application_type set to native in the generated client metadata.
+    application_type: hostname === "localhost" ? "native" : "web",
+    dpop_bound_access_tokens: true,
+  };
   clientMetadataSchema.parse(meta);
   return new ReactNativeOAuthClient({
     handleResolver: "https://bsky.social", // backend instances should use a DNS based resolver
