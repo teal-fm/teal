584041b46432e25c0546816ba944c0b5d869692b
Natalie B <22222885+espeon@users.noreply.github.com>
2025-05-10 20:28:05 -0500

    update React-navigation along with expo-router

--- apps/amethyst/app/offline.tsx
+++ apps/amethyst/app/offline.tsx
@@ -1,13 +1,30 @@
-import { View } from 'react-native';
-import { Text } from '@/components/ui/text';
-import { Icon } from '@/lib/icons/iconWithClassName';
-import { CloudOff } from 'lucide-react-native';
+import { View } from "react-native";
+import { Text } from "@/components/ui/text";
+import { Icon } from "@/lib/icons/iconWithClassName";
+import { CloudOff } from "lucide-react-native";
+import { Link, Stack } from "expo-router";
 
 export default function Offline() {
   return (
     <View className="flex-1 justify-center items-center gap-2">
-      <Icon icon={CloudOff} size={64} />
-      <Text className="text-center">Oops! Can’t connect to teal.fm.</Text>
+      <Stack.Screen
+        options={{
+          title: "Can't Connect",
+          headerBackButtonDisplayMode: "minimal",
+        }}
+      />
+      <View className="max-w-md justify-center items-center">
+        <Icon icon={CloudOff} size={64} />
+        <Text className="text-center">Oups! Can’t connect to teal.fm.</Text>
+        <Text className="text-center">
+          Try again in a few seconds, or you can change the AppView{" "}
+          <Link href="/settings">
+            <Text className="text-blue-600 dark:text-blue-400 border-b border-border">
+              in Settings
+            </Text>
+          </Link>
+        </Text>
+      </View>
     </View>
   );
 }
--- apps/amethyst/babel.config.js
+++ apps/amethyst/babel.config.js
@@ -1,20 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo"],
-      "nativewind/babel",
-    ],
+    presets: [["babel-preset-expo"], "nativewind/babel"],
     plugins: [
-      ['babel-plugin-react-compiler', { target: '18' }],
+      ["babel-plugin-react-compiler", { target: "18" }],
       [
-        'module-resolver',
+        "module-resolver",
         {
           alias: {
             "@": "./",
-            "~": "./"
+            "~": "./",
           },
-        }
+        },
       ],
       [
         "@babel/plugin-transform-react-jsx",
@@ -23,8 +20,8 @@ module.exports = function (api) {
           importSource: "nativewind",
         },
       ],
-      '@babel/plugin-transform-export-namespace-from',
-      'react-native-reanimated/plugin'
-    ]
+      "@babel/plugin-transform-export-namespace-from",
+      "react-native-reanimated/plugin",
+    ],
   };
 };
