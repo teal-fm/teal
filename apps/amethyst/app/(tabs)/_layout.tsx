bb0f9bd84029271c39f9a0ed2fbc3d8b05a8303b
Kyle Loveless <kjloveless321@gmail.com>
2025-05-18 23:04:11 -0500

    lil fix

--- apps/amethyst/app/(tabs)/_layout.tsx
+++ apps/amethyst/app/(tabs)/_layout.tsx
@@ -108,7 +108,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="/profile/undefined"
+        name="profile/[handle]"
         options={{
           title: "Stamp",
           tabBarIcon: ({ color }) => (
